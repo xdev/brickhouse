a32e44487728363a60920f95e4b94ff2adc5e841
Joshua Rudd <joshua@bricksf.com>
2008-12-15 12:01:17 -0800

    Fix front controller to properly use getInstance and not call its parent constructor

--- _example_app/controllers/_ControllerFront.php
+++ _example_app/controllers/_ControllerFront.php
@@ -5,7 +5,17 @@ class _ControllerFront extends ControllerFront
 	
 	private function __construct()
 	{
-		parent::__construct();
+		self::setUri();
+	}
+	
+	//override the singleton constructor
+	public static function getInstance()
+	{
+		if(!self::$instance){
+			$c = __CLASS__;
+			self::$instance = new $c();
+		}
+		return self::$instance;
 	}
 	
 }
